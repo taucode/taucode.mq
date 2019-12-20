e0eb794998212d2026b8be0b50d8b9e62cd3a456
Andrii Kovalenko <Andrii@invictusgurus.com>
2019-12-20 15:49:38 +0200

    Dummy fixture for Jenkins.

---
+++ tests/TauCode.Mq.EasyNetQ.Tests/DummyMqEasyNetQFixture.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TauCode.Mq.EasyNetQ.Tests
+{
+    [TestFixture]
+    public class DummyMqEasyNetQFixture
+    {
+        [Test]
+        public void DummyMqEasyNetQTest()
+        {
+            Assert.Pass("Added for Jenkins compatibility");
+        }
+    }
+}
