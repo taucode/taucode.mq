4ccca9591cc357364f04c64a8df4c412aa61bc88
Andrey Kovalenko <ak@taucode.com>
2022-08-12 04:01:06 +0300

    Bumped dev version to 1.5.5 alpha.

--- pre-deploy.cmd
+++ pre-deploy.cmd
@@ -3,7 +3,7 @@ dotnet restore
 dotnet build --configuration Debug
 dotnet build --configuration Release
 
-dotnet test -c Debug .\tests\TauCode.Mq.Tests\TauCode.Mq.Tests.csproj
-dotnet test -c Release .\tests\TauCode.Mq.Tests\TauCode.Mq.Tests.csproj
+dotnet test -c Debug .\test\TauCode.Mq.Tests\TauCode.Mq.Tests.csproj
+dotnet test -c Release .\test\TauCode.Mq.Tests\TauCode.Mq.Tests.csproj
 
 nuget pack nuget\TauCode.Mq.nuspec
